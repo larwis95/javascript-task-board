5a4d5fd6a736589cb0879eaf715be86910068d6a
Larry Wisniewski <larryw48390@gmail.com>
2024-03-07 16:47:10 -0500

    fixed multiple bugs, optimized suboptimal code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,65 +8,114 @@ const doneUl = $('#done-list');
 const taskName = $('#taskName');
 const taskDate = $('#taskDate');
 const taskDesc = $('#taskDescription');
-const colorDate = 5;
+const breakpointDate = 5;
+const classes = {
+    greenCard: "card text-white bg-success mb-3",
+    greenBtn: 'btn btn-success btn-outline-light',
+    yellowCard: 'card text-dark bg-warning mb-3',
+    yellowBtn: 'btn btn-warning btn-outline-dark',
+    redCard: 'card text-white bg-danger mb-3',
+    redBtn: 'btn btn-danger btn-outline-light'
+};
 let savedCards = [];
-let savedLists = [];
+let orderList = [];
 
-// Todo: create a function to generate a unique task id
+class card {
+    constructor(colorCheck) {
+        this.colorCheck = colorCheck;
+    }
+
+    checkColor() {
+        if (this.colorCheck === 'green') {
+            return classes.greenCard;
+        }
+        else if (this.colorCheck === 'yellow') {
+            return classes.yellowCard;
+        }
+        else {
+            return classes.redCard;
+        }
+    }
+};
+class button extends card {
+    constructor(colorCheck) {
+    super(colorCheck)
+    }
+    
+    checkColor() {
+        if (this.colorCheck === 'green') {
+            return classes.greenBtn;
+        }
+        else if (this.colorCheck === 'yellow') {
+            return classes.yellowBtn;
+        }
+        else {
+            return classes.redBtn;
+        }
+    }
+};
+// generate taskids
 function generateTaskId() {
     let id = dayjs().unix() + Math.random();
     return id;
 };
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
-    todoUl.append(`<li id="${task.id}"><div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">${task.name}</h5>
-      <h6 class="card-subtitle">Due Date: ${dayjs(`${task.date}`).format('MMM D, YYYY')}</h6>
-      <p class="card-text">${task.desc}</p>
-      <button>Delete</button>
-     </div>
-  </div></li>`);
-  handleDrag();
-  taskName.val('');
-  taskDate.val('');
-  taskDesc.val('');
+function checkDate(date) {
+    const today = dayjs();
+    const taskDate = dayjs(date);
+    const daysLeft = taskDate.diff(today, 'days');
+    console.log(daysLeft);
+    if (daysLeft > breakpointDate) {
+        return 'green';
+    }
+    else if (daysLeft <= breakpointDate && daysLeft >= 0) {
+        return 'yellow';
+    }
+    else {
+        return 'red'
+    };
 };
 
-// Todo: create a function to render the task list and make cards draggable
+// creates the taskcards
+function createTaskCard(task, list) {
+    const cardColor = new card(task.color);
+    const btnColor = new button(task.color);
+    list.append(`<li id="${task.id}"><div class="${cardColor.checkColor()}" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${task.name}</h5>
+      <h6 class="card-subtitle">${dayjs(`${task.date}`).format('MMM D, YYYY')}</h6>
+      <p class="card-text">${task.desc}</p>
+      <button class="${btnColor.checkColor()}"}>Delete</button>
+     </div>
+  </div></li>`);
+  updateTaskOrder();
+};
+
+// renders our lists
 function renderTaskList() {
-    let allList = $('ul');
-    for (let i = 0; i < savedLists.length; i++) {
-        allList[i].innerHTML = savedLists[i];
-    };
-    let allLi = $(allList.children());
-        for (let i = 0; i < savedCards.length; i++) {
-                let today = dayjs();
-                let dueDate = dayjs(savedCards[i].date)
-                console.log(`due date days left: ${dueDate.diff(today, 'days')}`)
-                if ($(allLi[i]).attr('id') == savedCards[i].id) {
-                    debugger;
-                    console.log(allLi[i]);
-                if ((dueDate.diff(today, 'days') < colorDate && dueDate.diff(today, 'days') >= 0) && $(allLi[i]).parent().attr('id') !== 'done-list') {
-                    $(allLi[i].children[0]).attr('class', 'card text-dark bg-warning mb-3');
-                    $(allLi[i].children[0].children[0].children[3]).attr('class', 'btn btn-warning btn-outline-dark');
-                }
-                else if (dueDate.diff(today, 'days') < 0 && $(allLi[i]).parent().attr('id') !== 'done-list') {
-                    $(allLi[i].children[0]).attr('class', 'card text-white bg-danger mb-3');
-                    $(allLi[i].children[0].children[0].children[3]).attr('class', 'btn btn-danger btn-outline-light');
-                }
-                else if ($(allLi[i]).parent().attr('id') === 'done-list' || dueDate.diff(today, 'days') >= colorDate) {
-                    $(allLi[i].children[0]).attr('class', 'card text-white bg-success mb-3');
-                    $(allLi[i].children[0].children[0].children[3]).attr('class', 'btn btn-success btn-outline-light');
-                
-                }
-                else {
-                    return;
-                }
-            };
-        };
-    };
+    const allList = $('ul');
+    allList.empty();
+    debugger;
+    const todoIds = orderList[0].map(Number);
+    const inprogIds = orderList[1].map(Number);
+    const completeIds = orderList[2].map(Number);
+    console.log(savedCards.length);
+    for (let i = 0; i < savedCards.length; i++) {
+        if (todoIds.includes(savedCards[i].id) === true) {
+        createTaskCard(savedCards[i], todoUl);
+        }
+        if (inprogIds.includes(savedCards[i].id) === true) {
+        createTaskCard(savedCards[i], inProgressUl);
+        }
+        if (completeIds.includes(savedCards[i].id) === true) {
+        savedCards[i].color = 'green'
+        createTaskCard(savedCards[i], doneUl);
+        }
+    }
+    updateTaskOrder();
+};
+
+
 
 // Todo: create a function to handle adding a new task
 function handleAddTask() {
@@ -76,11 +125,13 @@ function handleAddTask() {
         date: taskDate.val(),
         desc: taskDesc.val(),
         id: generateTaskId(),
+        color: ''
     };
+    task.color = checkDate(task.date);
     savedCards.push(task);
     localStorage.setItem('tasks', JSON.stringify(savedCards));
     formModal.modal('toggle');
-    createTaskCard(task);
+    createTaskCard(task, todoUl);
 };
 
 // Todo: create a function to handle deleting a task
@@ -91,37 +142,55 @@ function handleDeleteTask(event) {
     for (let i = 0; i < savedCards.length; i++) {
         if (savedCards[i].id == liId) {
             savedCards.splice(i, 1);
-            localStorage.setItem("tasks", JSON.stringify(savedCards));
             li.remove();
         };
     };
-    handleDrag();
-    
+    localStorage.setItem("tasks", JSON.stringify(savedCards));
+    updateTaskOrder();
 };
 
-function handleDrag() {
-    let allList = $('ul');
-    savedLists = [];
-    for (let i = 0; i < allList.length; i++) {
-        savedLists.push(allList[i].innerHTML);
-        renderTaskList();
-    };
-    localStorage.setItem("taskOrder", JSON.stringify(savedLists));
-};
+function updateTaskOrder() {
+    orderList = [];
+    orderList = [todoUl.sortable('toArray', {attribute: 'id'}), inProgressUl.sortable('toArray', {attribute: 'id'}), doneUl.sortable('toArray', {attribute: 'id'})]
+    localStorage.setItem('taskOrder', JSON.stringify(orderList));
+}
+
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(() => {
     const submitForm = $('#card-form');
     const deleteTaskBtn = $('#lists-container');
     const lists = $('ul.connected-list');
-    lists.sortable({
+    lists.sortable(
+    {
     connectWith: ".connected-list",
     placeholder: "ui-state-highlight",
-    update: () => {
-        handleDrag();
-    }}).disableSelection();
+    stop: (event, ui) => {
+        orderList = [todoUl.sortable('toArray', {attribute: 'id'}), inProgressUl.sortable('toArray', {attribute: 'id'}), doneUl.sortable('toArray', {attribute: 'id'})]
+        let li = $(ui.item);
+        let ul = li.closest('ul');
+        let btn = li.find('button');
+        let cardDiv = li.children()
+        console.log(cardDiv);
+        localStorage.setItem('taskOrder', JSON.stringify(orderList));
+        if (ul.attr('id') === 'done-list') {
+            cardDiv.attr('class', classes.greenCard);
+            btn.attr('class', classes.greenBtn);
+        }
+        else if (ul.attr('id') !== 'done-list') {
+            let dateText = li.find('h6').text;
+            let formatedDate = dayjs(dateText).format('YYYY MM DD')
+            let colorCheck = checkDate(formatedDate);
+            const cardColor = new card(colorCheck)
+            const btnColor = new button(colorCheck);
+            cardDiv.attr('class', cardColor.checkColor());
+            btn.attr('class', btnColor.checkColor());
+        }
+      }
+    }
+    ).disableSelection();
     if (taskOrder !== null) {
-        savedLists = taskOrder;
+        orderList = taskOrder;
     };
     if (taskList !== null) {
         savedCards = taskList;
